2e3f7da6c5ea2b6982a7fcaa859cfb8f397deaa2
Siva Alamalakala <leo.shiv@gmail.com>
2023-05-04 15:55:20 +0530

    minor change

--- quiz_app/lib/summary_item.dart
+++ quiz_app/lib/summary_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/question_identifier.dart';
 
