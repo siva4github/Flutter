4a91cee6ddd0b54e5f34aac8fe84e5619762e25d
Siva Alamalakala <leo.shiv@gmail.com>
2023-05-07 17:46:23 +0530

    updated app with statefulwidget and some basic changes

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:expense_tracker_app/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Text('Test'),
+      home: Expenses(),
     ),
   );
 }
