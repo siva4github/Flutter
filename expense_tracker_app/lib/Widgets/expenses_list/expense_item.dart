bbb6244670c80fdc641ab4eca66441a1d154c9c6
Siva Alamalakala <leo.shiv@gmail.com>
2023-05-07 19:58:02 +0530

    used Card and Spacer to display expense item

---
+++ expense_tracker_app/lib/Widgets/expenses_list/expense_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
