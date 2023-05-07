b2815914a9bac2d393f3d817b98161fc1d30c012
Siva Alamalakala <leo.shiv@gmail.com>
2023-05-07 19:37:51 +0530

    Used Expanded Widget inside Column widget to show the List

--- expense_tracker_app/lib/expenses.dart
+++ expense_tracker_app/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker_app/expenses_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,13 +12,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('Tha Chart'),
-          Text('Expenses List'),
+        children: [
+          const Text('Tha Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
         ],
       ),
     );
