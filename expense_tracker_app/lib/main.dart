393c183a7292b59fe0c69908d81b454619d663a5
Siva Alamalakala <leo.shiv@gmail.com>
2023-05-13 20:57:08 +0530

    using dark theme

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -4,9 +4,26 @@ import 'package:flutter/material.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 59, 173, 181));
 
+var kDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 151, 15, 6));
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
@@ -31,6 +48,7 @@ void main() {
               ),
             ),
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
